ecbd8300c6ee6a5ee1bf8ce61c3bf36900a94fc1
xvsingue <Singue919@hotmail.com>
2025-11-16 15:11:58 +0800

    正儿八经优化为通用视图了

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,45 +1,24 @@
-from django.shortcuts import (
-    HttpResponse,
-    render,
-    get_object_or_404,
-    HttpResponseRedirect,
-)
-from django.template import loader
-from .models import Question, Choice
-from django.http import Http404
 from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-# 展示数据库里以发布日期排序的最近 5 个投票问题，以空格分割
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {"latest_question_list": latest_question_list}
-#     return HttpResponse(template.render(context, request))
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, "polls/detail.html", {"question": question})
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
 def vote(request, question_id):
